5a8bc3df38802af5c7e3a3c41443351216ac44fb
codewitgabi <gabrielmicheal497@gmail.com>
2023-11-06 14:48:50 +0100

    Fixed code errors

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,13 +1,13 @@
 var bubbleSort = function (arr) {
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = 0; j < arr.length - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                var temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
-    return arr;
+  }
+  return arr;
 };
 console.log(bubbleSort([5, 4, 3, 2, 1]));
